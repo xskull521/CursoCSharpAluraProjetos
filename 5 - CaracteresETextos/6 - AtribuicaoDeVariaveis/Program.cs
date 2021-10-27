38773052ba3f191a1a0a7e409cd78734abf525a3
Higor Morais Sousa dos Anjos <higor.anjos@thomasgreg.corp>
2021-10-27 13:04:41 -0300

    6 ao 09

---
+++ 5 - CaracteresETextos/6 - AtribuicaoDeVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6___AtribuicaoDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("executando o projeto 6");
+
+            int idade = 32;
+            int idadeHigor = idade;
+
+            Console.WriteLine(idadeHigor);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
