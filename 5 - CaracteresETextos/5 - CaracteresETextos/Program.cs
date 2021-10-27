09048904c16cf1707ec603578f5814f12173866c
Higor Morais Sousa dos Anjos <higor.anjos@thomasgreg.corp>
2021-10-27 11:46:36 -0300

    Aula 5 caracteres e textos

---
+++ 5 - CaracteresETextos/5 - CaracteresETextos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titulo = "Alura Cursos de Tecnologia" + 2021;
+            string cursosProgramacao =
+       @"- .Net
+Java
+Javascript";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+            Console.ReadLine();
+
+        }
+    }
+}
