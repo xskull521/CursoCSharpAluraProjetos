6973887f1d7a7645c9962cbf4972face07542d9d
higoranjos <higor.anjos@thomasgreg.com.br>
2021-11-09 18:41:41 -0300

    Aula 09.11.21

---
+++ ContinuacaoByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,69 @@
+﻿//using _05_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100;
+
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+              
+            }
+            else
+            {
+                this.saldo = saldo;
+            }
+        }
+        
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+
+    }
+
+}
+
+
+   
